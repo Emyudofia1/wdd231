f7880756d7bcbf08b50bd823ac572f1bb6b50213
emyudofia1 <emyudofia1@gmail.com>
2025-05-09 12:13:11 -0600

    Course home page done

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -11,3 +11,22 @@ function filterCourses(type) {
 
 // Display last updated date
 document.getElementById("lastUpdate").textContent = new Date().toLocaleString();
+
+
+{/* <script> */}
+function filterCourses(dept) {
+    const courses = document.querySelectorAll('.course');
+    const buttons = document.querySelectorAll('.buttons button');
+
+    buttons.forEach(btn => btn.classList.remove('active'));
+    event.target.classList.add('active');
+
+    courses.forEach(course => {
+        if (dept === 'all' || course.classList.contains(dept)) {
+            course.style.display = 'block';
+        } else {
+            course.style.display = 'none';
+        }
+    });
+}
+
